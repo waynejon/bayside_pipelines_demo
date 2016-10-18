5bbe543082b8a84b26c76d96fa59b02c3fd30517
Pavithra Raman <pavithra.raman@acquia.com>
2016-10-18 14:27:33 -0400

    update test.sh

--- test-setup.sh
+++ test-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "Start Drupal Site Install"
 mysql -u root -proot -e "CREATE DATABASE drupal"
 cd docroot
 /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" ../bin/drush site-install -y --db-url=mysql://root:root@127.0.0.1:3306/drupal
 php -S 127.0.0.1:8081 &
 sleep 4
 cd ..
+echo "Completed Drupal Site Install"
